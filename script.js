9fcce99113e3f08cc8163f99d2d320dba8bd706e
Roby27 <rob.branni@gmail.com>
2024-07-06 23:42:12 +0200

    FIX: indent

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let htmlElement = document.documentElement
-            let toggleThemeElement = document.getElementById('toggletheme')
+let toggleThemeElement = document.getElementById('toggletheme')
             
-            toggleThemeElement.addEventListener('click', function () {
-                let theme = htmlElement.getAttribute('data-bs-theme')
-                if (theme === 'light') {
-                    htmlElement.setAttribute('data-bs-theme', 'dark');
-                    toggleThemeElement.innerHTML = "<i class=\"bi bi-brightness-high-fill\"></i>"
-                } else {
-                    htmlElement.setAttribute('data-bs-theme', 'light');
-                    toggleThemeElement.innerHTML = "<i class=\"bi bi-moon-fill\"></i>"
-                }
-            })
+toggleThemeElement.addEventListener('click', function () {
+    let theme = htmlElement.getAttribute('data-bs-theme')
+    if (theme === 'light') {
+    htmlElement.setAttribute('data-bs-theme', 'dark');
+    toggleThemeElement.innerHTML = "<i class=\"bi bi-brightness-high-fill\"></i>"
+    } else {
+    htmlElement.setAttribute('data-bs-theme', 'light');
+    toggleThemeElement.innerHTML = "<i class=\"bi bi-moon-fill\"></i>"
+    }
+    })
